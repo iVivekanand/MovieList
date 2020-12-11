54e6ef5ed3da7f17135c3fbe5a6a8dcd409bfd08
Vivek <vivekanand.ilango@gmail.com>
2020-12-11 11:53:59 +0530

    Home url updated to default to admin interface

--- films/urls.py
+++ films/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin
 from django.urls import path, include
 
 from films.crons.database_refresh import start_db_refresh
 from films.views import home
 
 urlpatterns = [
+    path('', admin.site.urls),
     path('movies/', home, name='home'),
     path('movies/api/', include('films.api.urls')),
 ]
